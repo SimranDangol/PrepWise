9e9b9162076c5aa78b61638d506b6ca545df53a4
SimranDangol <simrandangol10@gmail.com>
2025-05-19 12:10:31 +0545

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,12 +8,19 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://prepwise-mu.vercel.app"],
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 //routes
